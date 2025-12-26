2fc6b2a55cf2fc5a875dfc887a45d6a9ddb5c21e
Shisui-1111 <castorpop30@gmail.com>
2025-12-26 17:50:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,10 @@
-const settings = {
+const settings = {KnightBot!H4sIAAAAAAAAA5VUXbOiOBD9L3nVGgER0apbtYoKiCh+o1v7ECFgFBJIAopT/vctvHPnzsPu7N230HR1nz7ndH8HhGKOHFSB/neQMVxCgeqnqDIE+mBYRBFioAlCKCDog611CnqR7e1z+RFew5uROodBQli6OAyskraoup6Witq4T6Q38GyCrDglOPhNQQK7WGXrXnyZT4l51P00MQS8p4sgCcTEE49gU/jSVtuS+A0864oQM0zicXZGKWIwcVDlQcy+Bh8utJTLtxmU1YUVyxtTj/PdIndCv5GusamuUGNvKaczS9SvwT+LyWT3SFnjpnOlOi7wQaVWVJ73enk+2p3JbV12pIWlu+vBO3yOY4JCO0REYFF9mfe9K4UbjI6aZRJI3bsXi6VuWQd88Ixx5TdaEgmle2/Or9evAd89cNU55w6/4GyA89HRP2iNa9a7dUYk7nFVhdnyMmxsJv7tV+Ae+/DK9f/w7ozKzjDLpE5ato/JIfIfwm8Y0mg5yKyVF1zP1eJy2JqNe7L9GvwGn8dH3kqZ00nyzeNmT09YcXd5lbUPU9+33G1J9sXjtiT2J3woCvY7lKdOLo5Lh3rkwWflehUmQbc6G/vZNJTOhtxrzd1bOhq2GqvTnOCHEYUXmrSubWXhdKgy3Zgr9dFpnBoq8kZ6pbZtT4F4+faa6IoqOwR9+dkEDMWYCwYFpqSOae0mgGG5RgFD4sUuUJhHdYI3fndrmKUfL/f+NOS0rSvdsTAuLSpolsd7sz1T30ATZIwGiHMUWpgLyioXcQ5jxEH/z7+agKC7eNet7taWmyDCjIstKbKEwvBD1I+fMAhoQcS6IoFRPxADfekzjITAJOY1jQWBLDjjEhlnKDjoRzDh6OeAiKEQ9AUr0M+lNWhY8653DcvS5ypogvSlBw7rmVVF0rSeIrfbSr/T/YN/u9VlYZZ9I0iAJkheabIkS5qmKZLa7cntOrGOP38CrOuFSECccNAHxtTMy1YwGU99teDUNAfjeGDEA/A50Icx3pkPy+k1R9v7Mr11d49tWmZZNFGVRoyVeIWCfO44BR3G7kWK3/6hCOgDtTRzD2+nWum6q43pY/1g6BdTaslOT6LBtoWxuzOlgKRRtaqM6qG0rjM1epR2j83K1qLLILIf2wkPwyOLqTPyXc2oXdQEISpxgH5tZgaFjPfjo7fxkk0kdW5uPpxMNnm17GSxG5rH3XRXLKC5bOlLdClPHJ2STrqy9aiw75fdwXYDp32doMK0Rq243IyuM+9ye7fsa2WSH6cKv9xUS1V/Rhi9Np/AWsD/lu4deO0w6dn8pcaPW/Iv+zj0T9XRIYm7nfKFHMhTpkH/juYltQUd+8S5rDHLB4eR3bPB8/lXE2QJFBFlaX3qScgoDkETMFrUlrVJRH/TzBja9iCOJ/XkCeRi8LkGG5wiLmCagb7c1bSu1tVU6T3LYzSzID/XJHjy4RyB598QHOygUwcAAA==
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋɴɪɢʜᴛ ʙᴏᴛ*
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: 242066921332 ', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
